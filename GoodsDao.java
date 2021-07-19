e4f0a034c377b9f510ae0a3cb01a9e03f45a6ffe
sisttest03 <rola70001@gmail.com>
2021-07-19 14:36:37 +0900

    goodsDao 생성 [김은영]

---
+++ GoodsDao.java
@@ -0,0 +1,13 @@
+class GoodsDao 
+{
+	public static void main(String[] args) 
+	{
+		public void insert(){
+			System.out.println("상품을 등록하였습니다.");
+		}
+
+		public void delete(){
+			System.out.println("상품을 삭제하였습니다.");
+		}
+	}
+}
